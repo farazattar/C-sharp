555b0fc068284b24a21208394ca603a1b1056bdf
farazattar <63309056+farazattar@users.noreply.github.com>
2022-09-12 15:03:38 +0430

    Delete From Database

---
+++ DeleteFromDatabse/DeleteFromDatabse/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteFromDatabse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=DCS34-PC\DCS34;Initial Catalog=Test;Integrated Security=True";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string sql = "";
+            string idNew = "'a03ot3'";
+            sql = "DELETE IP WHERE ID=" + idNew;
+            command = new SqlCommand(sql, cnn);
+            adapter.DeleteCommand = new SqlCommand(sql, cnn);
+            adapter.DeleteCommand.ExecuteNonQuery();
+            MessageBox.Show("Data successfully deleted !");
+            command.Dispose();
+            cnn.Close();
+        }
+    }
+}

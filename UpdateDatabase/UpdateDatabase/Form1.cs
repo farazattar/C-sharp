bd2cff8bb0bd7630cc4d6e0fcc7dd24eeeea1aed
farazattar <63309056+farazattar@users.noreply.github.com>
2022-09-12 14:58:53 +0430

    Update Database

---
+++ UpdateDatabase/UpdateDatabase/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=DCS34-PC\DCS34;Initial Catalog=Test;Integrated Security=True";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string sql = "";
+            string idNew = "'a03ot3'";
+            string ipNew = "'142.124.31.3'";
+            sql = "UPDATE IP SET IP=" + ipNew + "WHERE ID=" + idNew ;
+            command = new SqlCommand(sql, cnn);
+            adapter.UpdateCommand = new SqlCommand(sql, cnn);
+            adapter.UpdateCommand.ExecuteNonQuery();
+            MessageBox.Show("Data successfully modified !");
+            command.Dispose();
+            cnn.Close();
+        }
+    }
+}

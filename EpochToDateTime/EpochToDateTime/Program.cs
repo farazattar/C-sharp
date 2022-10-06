11d190e8a51573c07be9ae8cf934b52c883dc487
farazattar <63309056+farazattar@users.noreply.github.com>
2022-10-06 13:38:14 +0330

    Unix Time to Date Time Converter

---
+++ EpochToDateTime/EpochToDateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EpochToDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UnixTime = Convert.ToInt32(args[0]);
+            DateTimeOffset UtcDateTime = DateTimeOffset.FromUnixTimeSeconds(UnixTime);
+            DateTime DtUtcDateTime = UtcDateTime.DateTime;
+            TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+            DateTime LocalDateTime = TimeZoneInfo.ConvertTimeFromUtc(DtUtcDateTime, TimeZone);
+            Console.WriteLine("UTC Date Time is {0}.", UtcDateTime);
+            Console.WriteLine("Local Date Time (with Time Zone) is {0}.", LocalDateTime);
+        }
+    }
+}

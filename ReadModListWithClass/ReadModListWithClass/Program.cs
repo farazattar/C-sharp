f8627e705ac3a693065ba47396e17c95f90e5bf8
farazattar <63309056+farazattar@users.noreply.github.com>
2022-10-17 15:07:56 +0330

    Read MOD List- With Classes

---
+++ ReadModListWithClass/ReadModListWithClass/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ReadModListWithClass
+{
+    struct ModIndexContent
+    {
+        public int ModPrefix;
+        public int ModNum;
+        public int ModTav;
+        public int WriteTime;
+        public int MSecWriteTime;
+        public int Zero1;
+        public int Zero2;
+        public int Zero3;
+        public int Zero4;
+        public int StartTime;
+        public int MSecStartTime;
+        public int EndTime;
+        public int MSecEndTime;
+        public int StartTime2;
+        public int MSecStartTime2;
+        public int EndTime2;
+        public int MSecEndTime2;
+        public int Frei;
+        public int Frei2;
+        public int Zero5;
+        public int Zero6;
+    };
+    class UnixTimeToDataTimeConverter
+    {
+        public static DateTime EpochToDateConverter(int UnixTime)
+        {
+            DateTimeOffset UtcDateTime = DateTimeOffset.FromUnixTimeSeconds(UnixTime);
+            DateTime DtUtcDateTime = UtcDateTime.DateTime;
+            TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+            DateTime LocalDateTime = TimeZoneInfo.ConvertTimeFromUtc(DtUtcDateTime, TimeZone);
+            return LocalDateTime;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"D:\g_in_pda_unit3";
+            const int ModIndexStart = 84;
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine("MOD List doesn't exist!");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            using (FileStream filestream = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                filestream.Seek(ModIndexStart, SeekOrigin.Begin);
+                ModIndexContent ModContent;
+                int ModIndexLenght = Marshal.SizeOf(typeof(ModIndexContent));
+                byte[] ReadBuffer = new byte[ModIndexLenght];
+                BinaryReader binaryreader = new BinaryReader(filestream);
+                for (int i = 0; i < (binaryreader.BaseStream.Length - ModIndexStart); i += ModIndexStart)
+                {
+                    ReadBuffer = binaryreader.ReadBytes(ModIndexLenght);
+                    GCHandle Handle = GCHandle.Alloc(ReadBuffer, GCHandleType.Pinned);
+                    ModContent = (ModIndexContent)Marshal.PtrToStructure(
+                    Handle.AddrOfPinnedObject(),
+                    typeof(ModIndexContent));
+                    Handle.Free();
+                    Console.WriteLine("MOD Number is: {0}",ModContent.ModNum);
+                    Console.WriteLine("MOD Tav is: {0}",ModContent.ModTav);
+                    Console.WriteLine("Start time is: {0}", UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.StartTime));
+                    Console.WriteLine("End Time is: {0}", UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.EndTime));
+                    Console.WriteLine("Write Time is: {0}", UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.WriteTime));
+                }
+            }
+        }
+    }
+}

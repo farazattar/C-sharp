758dbbb050b51dc5476d472b3743353dde052863
farazattar <63309056+farazattar@users.noreply.github.com>
2022-10-17 15:24:49 +0330

    Read MOD List- Clean

---
+++ ReadModListClean/ReadModListClean/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadModListClean
+{
+    static class Globals
+    {
+        public const int ModIndexStart = 84;
+        public static string FilePath = @"D:\g_in_pda_unit3";
+        public static string DcsTimeZone = "Central Europe Standard Time";
+    }
+}
---
+++ ReadModListClean/ReadModListClean/StructReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ReadModListClean
+{
+    class StructReader
+    {
+        public static void ReadStruct(string FilePath, int Offset)
+        {   
+            using (FileStream filestream = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                filestream.Seek(Offset, SeekOrigin.Begin);
+                ModIndexContent ModContent;
+                int ModIndexLenght = Marshal.SizeOf(typeof(ModIndexContent));
+                byte[] ReadBuffer = new byte[ModIndexLenght];
+                using (BinaryReader binaryreader = new BinaryReader(filestream))
+                {
+                    for (int i = 0; i < (binaryreader.BaseStream.Length - Offset); i += Offset)
+                    {
+                        ReadBuffer = binaryreader.ReadBytes(ModIndexLenght);
+                        GCHandle Handle = GCHandle.Alloc(ReadBuffer, GCHandleType.Pinned);
+                        ModContent = (ModIndexContent)Marshal.PtrToStructure(
+                        Handle.AddrOfPinnedObject(),
+                        typeof(ModIndexContent));
+                        Handle.Free();
+                        Console.WriteLine("Mod: {0}\t"+
+                                          "Tav: {1}\t"+
+                                          "From: {2}\t"+
+                                          "To: {3}\t"+
+                                          "Written at: {4}\t",                                            ModContent.ModNum,
+                                          ModContent.ModTav,
+                                          UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.StartTime, Globals.DcsTimeZone),
+                                          UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.EndTime, Globals.DcsTimeZone),
+                                          UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.WriteTime, Globals.DcsTimeZone));
+                    }
+                }
+            }
+        }
+    }
+}

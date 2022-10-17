635e8183f0e3b22ff8c184266c062b6c239a133a
farazattar <63309056+farazattar@users.noreply.github.com>
2022-10-17 15:10:47 +0330

    Read MOD List- With Extra Class

---
+++ ReadModListWithExtraClass/ReadModListWithExtraClass/UnixTimeToDataTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadModListWithExtraClass
+{
+    class UnixTimeToDataTimeConverter
+    {
+        public static DateTime EpochToDateConverter(int UnixTime)
+        {
+            DateTimeOffset UtcDateTime = DateTimeOffset.FromUnixTimeSeconds(UnixTime);
+            DateTime DtUtcDateTime = UtcDateTime.DateTime;
+            TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+            DateTime LocalDateTime = TimeZoneInfo.ConvertTimeFromUtc(DtUtcDateTime, TimeZone);
+            return LocalDateTime;
+        }
+    }
+}

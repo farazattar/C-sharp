98196d47bb988c141934c6a83876b133531b06dc
farazattar <63309056+farazattar@users.noreply.github.com>
2022-11-15 18:26:19 +0330

    WPF Radio Button

---
+++ WpfRadioButton/WpfRadioButton/ViewModel/ViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfRadioButton.Command;
+
+
+namespace WpfRadioButton.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+                public ICommand MyCommand { get; set; }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+                OnPropertyChange("Name");
+            }
+        }
+        
+
+        public ViewModel()
+        {
+            MyCommand = new RelayCommand(executemethod, canexecutemethod);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChange(string propertyname)
+        {
+            if (PropertyChanged !=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+
+
+        private bool canexecutemethod(object parameter)
+        {
+            if (parameter != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void executemethod(object parameter)
+        {
+            Name = (string)parameter;
+        }
+    }
+}
+

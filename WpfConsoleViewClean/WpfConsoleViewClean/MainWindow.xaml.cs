2d5868bb3b865595da7fe021015faf034c225e40
farazattar <63309056+farazattar@users.noreply.github.com>
2022-11-22 14:49:31 +0330

    WPF Console View according Clean
    WPF Console View according to Clean Code Principles

---
+++ WpfConsoleViewClean/WpfConsoleViewClean/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfConsoleViewClean.ConsoleView;
+
+namespace WpfConsoleViewClean
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            ConsoleManager.Show();
+            Console.WriteLine("Hi There!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            ConsoleManager.Hide();
+        }
+    }
+}

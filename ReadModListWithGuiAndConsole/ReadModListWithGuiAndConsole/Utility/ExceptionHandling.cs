b62691f91ae96513e693de8555098fb3d3ba5224
farazattar <63309056+farazattar@users.noreply.github.com>
2022-11-22 14:52:33 +0330

    Read MOD List With GUI and Console

---
+++ ReadModListWithGuiAndConsole/ReadModListWithGuiAndConsole/Utility/ExceptionHandling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadModListWithGuiAndConsole.Utility
+{
+    static class ExceptionHandling
+    {
+        public static void ExecptionOutput(Exception e)
+        {
+            Console.WriteLine("\n***Error Occured!***");
+            Console.WriteLine("Method: {0}", e.TargetSite);
+            Console.WriteLine("Message: {0}", e.Message);
+            Console.WriteLine("Source: {0}", e.Source);
+        }
+    }
+}

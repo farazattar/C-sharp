e446938688230e7f17933496287e5d0536ded2b4
farazattar <63309056+farazattar@users.noreply.github.com>
2022-11-22 14:27:39 +0330

    Read MOD List Unit 1

---
+++ ReadModListU1/ReadModListU1/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadModList
+{
+    static class Globals
+    {
+        public const int ModIndexStart = 84;
+        public static string UnitDataBase = "[ModList].[dbo].[Unit1]";
+        public static string InputFilePath = @"D:\g_in_pda_unit1";
+        public static string OutputFilePath = @"D:\ModList-Unit1.txt";
+        public static string DcsTimeZone = "Central Europe Standard Time";
+        public static string ConnectionString = "Data Source=DCS34-PC\\DCS34;Initial Catalog=ModList;Integrated Security=True";
+    }
+}

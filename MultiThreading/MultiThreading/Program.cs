d48945b1075fd373889620a0a24854a832e8d59b
farazattar <63309056+farazattar@users.noreply.github.com>
2022-12-15 12:00:09 +0330

    Multi Threading

---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+            Console.WriteLine(mainThread.Name);
+            Thread thread1 = new Thread(CountDown);
+            Thread thread2 = new Thread(CountUp);
+            thread1.Start();
+            thread2.Start();
+            //CountDown();
+            //CountUp();
+            Console.WriteLine(mainThread.Name + " is completed.");
+            Console.ReadKey();
+        }
+        public static void CountDown()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("Timer #1 is" + i + "seconds.");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #1 is completed.");
+        }
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Timer #2 is" + i + "seconds.");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #2 is completed.");
+        }
+    }
+}

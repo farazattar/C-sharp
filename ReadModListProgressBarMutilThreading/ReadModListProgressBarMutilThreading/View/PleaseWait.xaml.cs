8c2d5505d0af8fd99ee28bc3353780a5c6b3ea6a
farazattar <63309056+farazattar@users.noreply.github.com>
2022-12-15 12:03:37 +0330

    Read MOD List Progress Bar Multi Threading

---
+++ ReadModListProgressBarMutilThreading/ReadModListProgressBarMutilThreading/View/PleaseWait.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
+using ReadModListProgressBarMutilThreading.Utility;
+
+namespace ReadModListProgressBarMutilThreading.View
+{
+    /// <summary>
+    /// Interaction logic for PleaseWait.xaml
+    /// </summary>
+    public partial class PleaseWait : Window
+    {
+        public PleaseWait()
+        {
+            InitializeComponent();
+        }
+        public string InputFile = "";
+        public string OutputFile = "";
+        public string UnitDataBase = "";
+        private delegate void UpdateProgressBarDelegate(DependencyProperty dp, Object value); 
+        public void MainFunction()
+        {
+            try
+            {
+                StructReader.ReadStruct(InputFile, OutputFile, UnitDataBase, Globals.ModIndexStart);
+            }
+            catch (Exception err)
+            {
+                ExceptionHandling.ExecptionOutput(err);
+            }
+            finally
+            {
+                string MessageBoxMessage = "The MOD List exctracted from:\n" + InputFile +
+                    "\nis successfully inserted into the following database:\n" + UnitDataBase +
+                    "\nIn addition, the MOD List is writed into this file:\n" + OutputFile;
+                string MessageBoxTitle = "The program is successfully finished";
+                MessageBox.Show(MessageBoxMessage, MessageBoxTitle);
+            }
+        }
+        public void ProgressBarUpdate()
+        {
+            UpdateProgressBarDelegate updateProgressBar = new UpdateProgressBarDelegate(PleaseWaitProgressBar.SetValue); 
+            PleaseWaitProgressBar.Minimum = 0;
+            PleaseWaitProgressBar.Maximum = 100;
+            PleaseWaitProgressBar.Value = 0;
+            for (double i = 0; i < PleaseWaitProgressBar.Maximum; i++)
+            {
+                Dispatcher.Invoke(updateProgressBar,
+                    DispatcherPriority.Background,
+                    new object[] { ProgressBar.ValueProperty, i });
+                System.Threading.Thread.Sleep(1000);
+            }
+            PleaseWaitProgressBar.Value = PleaseWaitProgressBar.Maximum;
+        }
+    }
+}

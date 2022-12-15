ced214a14c76510ea763488d2b4a8ee7ee60dccf
farazattar <63309056+farazattar@users.noreply.github.com>
2022-12-15 12:07:02 +0330

    Read MOD List Progress Bar Background Worker New
    Almost Complete!!!

---
+++ ReadModListProgressBarBackgroundWorkerNew/ReadModListProgressBarBackgroundWorkerNew/Utility/StructReader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ReadModListProgressBarBackgroundWorkerNew.Utility
+{
+    struct ModIndexContent
+    {
+        public int ModPrefix;
+        public int ModNum;
+        public int ModTav;
+        public int WriteTime;
+        public int MSecWriteTime;
+        public int Zero1;
+        public int Zero2;
+        public int Zero3;
+        public int Zero4;
+        public int StartTime;
+        public int MSecStartTime;
+        public int EndTime;
+        public int MSecEndTime;
+        public int StartTime2;
+        public int MSecStartTime2;
+        public int EndTime2;
+        public int MSecEndTime2;
+        public int Frei;
+        public int Frei2;
+        public int Zero5;
+        public int Zero6;
+    };
+    class StructReader
+    {
+        public static void ReadStruct(string FilePath, string OutputFilePath, string UnitDataBase, int Offset)
+        {
+            Console.WriteLine("\nRead the input file . . .");
+            using (FileStream filestream = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                filestream.Seek(Offset, SeekOrigin.Begin);
+                ModIndexContent ModContent;
+                int ModIndexLenght = Marshal.SizeOf(typeof(ModIndexContent));
+                byte[] ReadBuffer = new byte[ModIndexLenght];
+                using (BinaryReader binaryreader = new BinaryReader(filestream))
+                {
+                    for (int i = 0; i < (binaryreader.BaseStream.Length - Offset); i += Offset)
+                    {
+                        ReadBuffer = binaryreader.ReadBytes(ModIndexLenght);
+                        GCHandle Handle = GCHandle.Alloc(ReadBuffer, GCHandleType.Pinned);
+                        ModContent = (ModIndexContent)Marshal.PtrToStructure(
+                        Handle.AddrOfPinnedObject(),
+                        typeof(ModIndexContent));
+                        Handle.Free();
+                        Console.WriteLine("\nWrite the MOD number {0} into the output file . . .", ModContent.ModNum);
+                        FileFunctions File = new FileFunctions();
+                        File.WriteToOutputFile(ModContent.ModNum,
+                                         ModContent.ModTav,
+                                         UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.StartTime, Globals.DcsTimeZone),
+                                         UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.EndTime, Globals.DcsTimeZone),
+                                         UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.WriteTime, Globals.DcsTimeZone),
+                                         OutputFilePath);
+                        Console.WriteLine("\nInsert the MOD number {0} into the database . . .", ModContent.ModNum);
+                        DataBaseFunctions DbFunctions = new DataBaseFunctions();
+                        DbFunctions.ConnectToDb(Globals.ConnectionString);
+                        DbFunctions.AddToDb(UnitDataBase,
+                                            ModContent.ModNum,
+                                            ModContent.ModTav,
+                                            ModContent.StartTime,
+                                            ModContent.EndTime,
+                                            ModContent.WriteTime,
+                                            UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.StartTime, Globals.DcsTimeZone),
+                                            UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.EndTime, Globals.DcsTimeZone),
+                                            UnixTimeToDataTimeConverter.EpochToDateConverter(ModContent.WriteTime, Globals.DcsTimeZone));
+                        DbFunctions.DisconnectFromDb();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ReadModListProgressBarBackgroundWorkerNew/ReadModListProgressBarBackgroundWorkerNew/View/PleaseWait.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
+using ReadModListProgressBarBackgroundWorkerNew.Utility;
+
+namespace ReadModListProgressBarBackgroundWorkerNew.View
+{
+    /// <summary>
+    /// Interaction logic for PleaseWait.xaml
+    /// </summary>
+    public partial class PleaseWait : Window
+    {
+        public PleaseWait()
+        {
+            InitializeComponent();
+            m_BackgroundWorker.WorkerReportsProgress = true;
+            m_BackgroundWorker.DoWork += BackgroundWorker_DoWork;
+            m_BackgroundWorker.ProgressChanged +=
+            BackgroundWorker_ProgressChanged;
+            m_BackgroundWorker.RunWorkerCompleted +=
+            BackgroundWorker_RunWorkerCompleted;
+        }
+        public string InputFile = "";
+        public string OutputFile = "";
+        public string UnitDataBase = "";
+        private delegate void UpdateProgressBarDelegate(DependencyProperty dp, Object value); 
+        BackgroundWorker m_BackgroundWorker = new BackgroundWorker();
+        public void MainFunction()
+        {
+            try
+            {
+                StructReader.ReadStruct(InputFile, OutputFile, UnitDataBase, Globals.ModIndexStart);
+            }
+            catch (Exception err)
+            {
+                ExceptionHandling.ExecptionOutput(err);
+            }
+            finally
+            {
+                string MessageBoxMessage = "The MOD List exctracted from:\n" + InputFile +
+                    "\nis successfully inserted into the following database:\n" + UnitDataBase +
+                    "\nIn addition, the MOD List is writed into this file:\n" + OutputFile;
+                string MessageBoxTitle = "The program is successfully finished";
+                MessageBox.Show(MessageBoxMessage, MessageBoxTitle);
+            }
+        }
+        public void RunTheProgram()
+        {
+            m_BackgroundWorker.RunWorkerAsync();
+        }
+        private void BackgroundWorker_DoWork(Object sender, DoWorkEventArgs e)
+        {
+            MainFunction();
+        }
+        private void BackgroundWorker_ProgressChanged(Object sender, ProgressChangedEventArgs e)
+        {
+            PleaseWaitProgressBar.Value = e.ProgressPercentage;
+        }
+        private void BackgroundWorker_RunWorkerCompleted(Object sender, RunWorkerCompletedEventArgs e)
+        {
+            Close();
+        }
+        public void ProgressBarUpdate()
+        {
+            UpdateProgressBarDelegate updateProgressBar = new UpdateProgressBarDelegate(PleaseWaitProgressBar.SetValue);
+            PleaseWaitProgressBar.Minimum = 0;
+            PleaseWaitProgressBar.Maximum = 100;
+            PleaseWaitProgressBar.Value = 0;
+            for (double i = 0; i < PleaseWaitProgressBar.Maximum; i++)
+            {
+                Dispatcher.Invoke(updateProgressBar,
+                    DispatcherPriority.Background,
+                    new object[] { ProgressBar.ValueProperty, i });
+                System.Threading.Thread.Sleep(1000);
+            }
+            PleaseWaitProgressBar.Value = PleaseWaitProgressBar.Maximum;
+        }
+    }
+}

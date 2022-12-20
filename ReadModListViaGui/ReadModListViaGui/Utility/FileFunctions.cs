33bcf84675a365ee426040839914f7ed1616c5c6
farazattar <63309056+farazattar@users.noreply.github.com>
2022-12-20 11:16:46 +0330

    Read MOD List via GUI
    First Release, nearly complete.
    This application insert the list into the database and write it into a file.

---
+++ ReadModListViaGui/ReadModListViaGui/Utility/FileFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ReadModListViaGui.Utility
+{
+    class FileFunctions
+    {
+        public static void CheckInputFile(string Inputfile)
+        {
+            if (!File.Exists(Inputfile))
+            {
+                Console.WriteLine("MOD List doesn't exist!");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+        public static void CreateOutputfile(string OutputFile)
+        {
+            if (File.Exists(OutputFile))
+            {
+                File.Delete(OutputFile);
+            }
+        }
+        public void WriteToOutputFile(int ModNum, int ModTav, DateTime StartTime, DateTime EndTime, DateTime WriteTime, string OutputFilePath)
+        {
+            using(StreamWriter FileWriter = File.AppendText(OutputFilePath))
+            {
+                FileWriter.WriteLine("Mod: {0}\t"+
+                                     "Tav: {1}\t"+
+                                     "From: {2}\t"+
+                                     "To: {3}\t"+
+                                     "Written at: {4}\t",                                            
+                                     ModNum,
+                                     ModTav,
+                                     StartTime,
+                                     EndTime,
+                                     WriteTime);
+            }
+        }
+    }
+}
---
+++ ReadModListViaGui/ReadModListViaGui/Utility/Globals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadModListViaGui.Utility
+{
+    static class Globals
+    {
+        public const int ModIndexStart = 84;
+        public static string Unit1DataBase = "[ModList].[dbo].[Unit1]";
+        public static string Unit2DataBase = "[ModList].[dbo].[Unit2]";
+        public static string Unit3DataBase = "[ModList].[dbo].[Unit3]";
+        public static string Unit4DataBase = "[ModList].[dbo].[Unit4]";
+        public static string DcsTimeZone = "Central Europe Standard Time";
+        public static string ConnectionString = "Data Source=DCS34-PC\\DCS34;Initial Catalog=ModList;Integrated Security=True";
+    }
+}

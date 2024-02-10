e7e946ce790aa4e057fc4cd7385b56d6b17c6037
farazattar <63309056+farazattar@users.noreply.github.com>
2024-02-10 12:25:09 +0330

    SINEC to Real Date and Time- V.1.0.0.0

---
+++ SinecToRealDateTime/SinecToRealDateTime/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SinecToRealDateTime.Utility;
+
+namespace SinecToRealDateTime
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ConvertButton_Clicked(object sender, RoutedEventArgs e)
+        {            
+            string SinecDateHex = SinecDateBox.Text;
+            string SinecTimeHex = SinecTimeBox.Text;
+            DateTime DateTimeResult = Sinec2Real.ConvertSinecToDateTime(SinecDateHex, SinecTimeHex);
+            string Result = DateTimeResult.ToString("dd-MM-yyyy  HH:mm:ss.fff");            
+            ResultBlock.Text = Result;
+        }
+        private void SinecDateBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "e.g. 3340")
+            {
+                textBox.Text = string.Empty;                
+            }
+        }
+        private void SinecDateBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "e.g. 3340";
+            }
+        }
+        private void SinecTimeBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "e.g. 030be07a")
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+        private void SinecTimeBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "e.g. 030be07a";
+            }
+        }
+    }
+}
---
+++ SinecToRealDateTime/SinecToRealDateTime/Utility/Sinec2Real.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinecToRealDateTime.Utility
+{
+    class Sinec2Real
+    {
+        public static DateTime ConvertSinecToDateTime(string SinecDateHex, string SinecTimeHex)
+        {
+            // Convert hex to uint
+            uint SinecDate = Convert.ToUInt32(SinecDateHex, 16);
+            uint SinecTime = Convert.ToUInt32(SinecTimeHex, 16);
+            
+            // SINEC time starts from January 1, 1984
+            DateTime BaseDate = new DateTime(1984, 1, 1);
+
+            // Convert days to seconds
+            uint SinecDaysToSeconds = SinecDate * 24 * 3600;
+            uint SinecTimeToSeconds = SinecTime / 1000;
+            uint SinecSeconds = SinecDaysToSeconds + SinecTimeToSeconds;
+            
+            // Convert seconds to TimeSpan
+            TimeSpan TotalTimeSpan = TimeSpan.FromSeconds(SinecSeconds);
+
+            // Add the TimeSpan to the base date
+            DateTime RealDateTime = BaseDate.Add(TotalTimeSpan);
+
+            return RealDateTime;
+        }
+    }
+}
